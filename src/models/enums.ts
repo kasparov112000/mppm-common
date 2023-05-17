e7a8aaee1ff5455b26e24a4220530d41f013a79f
Subramanyam Molleti <subramanyam.molleti@pwc.com>
2023-05-17 11:51:47 -0400

    Added Statuses Enum for Admin app

--- src/models/enums.ts
+++ src/models/enums.ts
@@ -1,15 +1,23 @@
 export enum StatusNamesEnum {
-    draft = 'draft',
-    initiated = 'initiated',
-    deleted = 'deleted',
-    shared = 'shared',
-    returnedReviewee = 'returnedReviewee',
-    returnedReviewer = 'returnedReviewer',
-    acknowledged = 'acknowledged',
-    review = 'review'
+  draft = 'draft',
+  initiated = 'initiated',
+  deleted = 'deleted',
+  shared = 'shared',
+  returnedReviewee = 'returnedReviewee',
+  returnedReviewer = 'returnedReviewer',
+  acknowledged = 'acknowledged',
+  review = 'review'
 }
 
 export enum TypeNamesEnum {
-    snapshot = 'snapshot',
-    reinvest = 'reinvest'
+  snapshot = 'snapshot',
+  reinvest = 'reinvest'
+}
+
+export enum AdminStatusNamesEnum {
+  New = 'New',
+  Draft = 'Draft',
+  ReadyToUse = 'Ready to use',
+  InUse = 'In use',
+  Archived = 'Archived',
 }
