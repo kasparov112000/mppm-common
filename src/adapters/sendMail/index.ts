594a53b03568db1fda7486fdd930e372f4911c55
Jereme Jackson <jereme.jackson@pwc.com>
2023-05-19 15:26:50 -0400

    update to add the ability to send emails

--- src/adapters/sendMail/index.ts
+++ src/adapters/sendMail/index.ts
@@ -1,16 +1,21 @@
 import { INotificationsConfig } from "../../models/inotificationsConfig";
 import * as sendGridMail from '@sendgrid/mail'
 
-sendGridMail.setApiKey(process.env.SENDGRID_API_KEY);
-
 export class SendMailAdapter {
+
+    private _sendgridFromEmail: string;
+
+    constructor (sendgridApiKey: string, sendgridFromEmail){
+        sendGridMail.setApiKey(sendgridApiKey);
+        this._sendgridFromEmail = sendgridFromEmail;
+    }
 
     async usingTemplate(toEmail: string, notificationTemplate: INotificationsConfig) {
         (async () => 
         { 
             await sendGridMail.send({
                 to: `${toEmail}`,
-                from: `${process.env.SENDGRID_FROM_EMAIL}`,
+                from: `${this._sendgridFromEmail}`,
                 subject: `${notificationTemplate.subjectLine}`,
                 html: `${notificationTemplate.body}`,
             }); 
