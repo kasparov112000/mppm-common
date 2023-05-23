07c79c653c0dbcc28668d02f0270d2757a8a6fda
Sameer Bhandari <sameer.bahndari@pwc.com>
2023-05-23 10:38:50 -0400

    added snapshot getByReviewerPartyId adapter

--- src/adapters/microservice-config.ts
+++ src/adapters/microservice-config.ts
@@ -3,6 +3,7 @@ import { MicroserviceConnection } from "../models/contracts/microservice";
 const microservices = {
     systemSettings: new MicroserviceConnection('http://system-settings', 3011, 'system-settings'),
     employees: new MicroserviceConnection('http://employees', 3004, 'employees', { byPartyId: 'byPartyId', byName: 'byName' }),
+    snapShot: new MicroserviceConnection('http://snapshots', 3001, 'snapshots', { byReviewerPartyId: 'byReviewerPartyId' }),
 }
 
 export {
