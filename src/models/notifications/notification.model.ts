b4b6773d6625001c855f1071a75ea7a4d5609874
Anirudh Kanukanti <anirudh.kanukanti@pwc.com>
2023-06-02 14:30:48 -0400

    Added Notification interface and updated event producer

--- src/models/notifications/notification.model.ts
+++
@@ -1,9 +0,0 @@
-import { NotificationDestinations } from "./notificationDestinations.enum"
-import { NotificationEmailPayload } from "./notificationEmailPayload.model"
-import { NotificationTypes } from "./notificationTypes.enum"
-
-export type Notification = {
-    notificationType: NotificationTypes,
-    destination: NotificationDestinations,
-    payload: NotificationEmailPayload
-}
---
+++ src/models/notifications/notificationStatus.enum.ts
@@ -0,0 +1,6 @@
+export enum NotificationStatuses {
+    Pending = 'Pending',
+    Processing = 'Processing',
+    Completed = 'Completed',
+    Failed = 'Failed'
+}
