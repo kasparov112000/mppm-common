20429a320a48bfa8e687f3d9e7d279dafd90d17d
Anirudh Kanukanti <anirudh.kanukanti@pwc.com>
2023-06-07 12:37:58 -0400

    removed unused fromAddress from the constructor

--- src/adapters/sendMail/index.ts
+++ src/adapters/sendMail/index.ts
@@ -1,13 +1,11 @@
 import { MailService, MailDataRequired, ClientResponse } from '@sendgrid/mail'
 
 export class SendGridEmailAdapter {
-    private _sendgridFromEmail: string;
     private _mailService: MailService;
 
-    constructor (sendgridApiKey: string, sendgridFromEmail: string) {
+    constructor (sendgridApiKey: string) {
       this._mailService = new MailService();
       this._mailService.setApiKey(sendgridApiKey);
-      this._sendgridFromEmail = sendgridFromEmail;
     }
 
     async sendEmail(mailData: MailDataRequired, isMultiple: boolean = false): Promise<ClientResponse> {
