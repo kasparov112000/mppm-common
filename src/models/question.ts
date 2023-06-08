4168d7bb834315b209889f93755d9baed0b56fb7
Subramanyam Molleti <subramanyam.molleti@pwc.com>
2023-06-07 22:47:57 -0400

    Added question model

---
+++ src/models/question.ts
@@ -0,0 +1,19 @@
+export interface Title {
+    default: string;
+    es: string;
+  }
+  
+  export interface Question {
+    _id?: string;
+    selected?: boolean;
+    type: string;
+    name: string;
+    inputMetric: string;
+    status: string;
+    title: Title;
+    questionText: string;
+    optlText: Title;
+    choices: any[];
+    isLock: boolean;
+  }
+  
