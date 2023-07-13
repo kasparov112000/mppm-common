6f09af9bb347154e4349836659ab834d9d096d6d
Jereme Jackson <jereme.jackson@pwc.com>
2023-07-13 14:56:54 -0400

    update to add logic to return dynamic errors for snapshot transitions

--- src/models/ierror.ts
+++ src/models/ierror.ts
@@ -1,7 +1,7 @@
 import * as mongoose from 'mongoose';
 import { ILanguageProperty } from './ilanguageProperty';
 
-export interface IErrors {
+export interface IError {
     _id?: mongoose.Schema.Types.ObjectId;
     type: string;
     code: string;
