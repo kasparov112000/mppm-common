ed85392e87c6224a79e23b73e013a932015af69c
Jereme Jackson <jereme.jackson@pwc.com>
2023-07-13 14:34:31 -0400

    update to add logic to return dynamic errors for snapshot transitions

--- src/models/custom-errors/transitionError.ts
+++ src/models/custom-errors/transitionError.ts
@@ -1,10 +1,15 @@
 import { TransitionResult } from '../../stateMachine/transitionResult';
+import { ILanguageProperty } from '../ilanguageProperty';
 
 export default class TransitionError extends Error {
     public readonly transitionResult: TransitionResult<any>;
+    public readonly transitionLanguage: ILanguageProperty;
+    public readonly errorCode: Number;
     
-    constructor(message: string, transitionResult: TransitionResult<any>) {
+    constructor(message: any, transitionResult: TransitionResult<any>, messageLanguage?: ILanguageProperty, errorCode?: Number) {
         super(message);
         this.transitionResult = transitionResult;
+        this.transitionLanguage = messageLanguage;
+        this.errorCode = errorCode;        
     }
 }
